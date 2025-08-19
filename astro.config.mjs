e3997b6625a8453fc7658ee6d96eb7b91ca8f938
FedeDiaz1981 <federicodiaz1981@gmail.com>
2025-08-19 12:07:20 -0300

    Se modifica index desactualizado

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import node from '@astrojs/node';
 
 export default defineConfig({
   integrations: [tailwind({ applyBaseStyles: true })],
@@ -9,5 +10,8 @@ export default defineConfig({
     routing: {
       prefixDefaultLocale: false,
     },
-  }
+  },
+    output: 'server',
+  adapter: node({ mode: 'standalone' }), // empaqueta server listo
+  server: { port: 3000, host: true },
 });
